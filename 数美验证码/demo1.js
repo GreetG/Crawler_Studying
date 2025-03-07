1710243da0b01f4b9714290bbc76007e028fcf7f
Greet <1736256448@qq.com>
2025-03-07 17:36:32 +0800

    Changes

--- 数美验证码/demo1.js
+++ 数美验证码/demo1.js
@@ -3,54 +3,47 @@
 var CryptoJS = require("crypto-js")
 function process_type(data){
     if (typeof data == "number"){
-        console.log(data)
         return data + ""
     }else{
         return JSON.stringify(data)
     }
 
 }
-function cul(hen,gui,org,rid){
+function cul(hen,gui,org,rid,time){
     var _0x23447b = {}
 
-    var gui = [[],[],[]]
-    var end_point = gui[gui.length-1]
-    var diff_tm = end_point[end_point.length - 1] - 0 + 31;
-    console.log(diff_tm)
-    testtm=[
-    [0, 4, 0],
-    [13, 0, 102],
-    [43, -2, 201]
-]
-    // _0x23447b['tb'] = getEncryptContent(hen / 300, '59fcff86');
-    _0x23447b['tb'] = getEncryptContent(0.14666666666666667, '59fcff86');
-    _0x23447b['tm'] = getEncryptContent(testtm, '0569c403');
-    // _0x23447b['ly'] = getEncryptContent(diff_tm, '65986a6b');
-    _0x23447b['ly'] = getEncryptContent(238, '65986a6b');
-    _0x23447b['fc'] = getEncryptContent(300, 'f1553d1a');
-    _0x23447b['uc'] = getEncryptContent(150, 'b0f132ec');
-    _0x23447b['act.os'] = 'web_pc';
-    _0x23447b['og'] = getEncryptContent(1, "aa3146c6");
-    _0x23447b['jp'] = getEncryptContent(0, "8584558c");
-    _0x23447b['aj'] = getEncryptContent(-1, "b295ccbf");
+    // var gui = [[],[],[]]
+    // var end_point = gui[gui.length-1]
+    // var diff_tm = end_point[end_point.length - 1] - 0 + 31;
+
+    _0x23447b['tb'] = getEncryptContent(hen / 300, '59fcff86');
+    _0x23447b['tm'] = getEncryptContent(gui, '0569c403');
+    _0x23447b['ly'] = getEncryptContent(time, '65986a6b');
+
+    _0x23447b['fc'] = getEncryptContent(300, 'f1553d1a');//匹配
+    _0x23447b['uc'] = getEncryptContent(150, 'b0f132ec');//匹配
+    _0x23447b['act.os'] = 'web_pc';//匹配???
+    _0x23447b['og'] = getEncryptContent(1, "aa3146c6");//匹配
+    _0x23447b['jp'] = getEncryptContent(0, "8584558c");//匹配
+    _0x23447b['aj'] = getEncryptContent(-1, "b295ccbf");//匹配
 
 
 
 
 
-    _0x23447b['sy'] = getEncryptContent("default", "7d9ab4ed");
-    _0x23447b['wz'] = getEncryptContent2("'DEFAULT'", "4a61a388");
+    _0x23447b['sy'] = getEncryptContent2("default", "7d9ab4ed");//匹配
+    _0x23447b['wz'] = getEncryptContent2("DEFAULT", "4a61a388");//匹配
 
 
-    _0x23447b['dj'] = getEncryptContent("zh-cn", "566e29c3");
-    _0x23447b['gp'] = getEncryptContent("11", "5300a156");
+    _0x23447b['dj'] = getEncryptContent2("zh-cn", "566e29c3");//匹配
+    _0x23447b['gp'] = getEncryptContent2("11", "5300a156");//匹配
     _0x23447b['rid'] = rid
-    _0x23447b["rversion"] = "1.0.4"
-    _0x23447b['sdkver'] = "1.1.3"
-    _0x23447b['protocol'] = "184"
-    _0x23447b['ostype'] = "web"
+    _0x23447b["rversion"] = "1.0.4"//匹配
+    _0x23447b['sdkver'] = "1.1.3"//匹配
+    _0x23447b['protocol'] = "184"//匹配
+    _0x23447b['ostype'] = "web"//匹配
     _0x23447b['organization'] = org
-    _0x23447b["callback"]="sm_1741286645601"
+    _0x23447b["callback"]="sm_1741337321131"
 
     return _0x23447b;
 }
@@ -79,4 +72,4 @@ function getEncryptContent2(data,key){
 }
 
 // cul()
-
+console.log(cul())
--- 数美验证码/数美滑块.py
+++ 数美验证码/数美滑块.py
@@ -1,6 +1,10 @@
 import json
+import math
 import random
+
+import cv2
 import execjs
+import numpy as np
 import requests
 import re
 import ddddocr
@@ -12,48 +16,56 @@ def download(url,save_path,session):
     img_response = session.get(url)
     with open(save_path,'wb') as f:
         f.write(img_response.content)
+def get_distance(fg, bg):
+    fg_image = np.asarray(bytearray(requests.get(url=fg, verify=False).content), dtype='uint8')
+    fg_image = cv2.imdecode(fg_image, 1)
+    shape = fg_image.shape
+    x_points, y_points = [], []
+    for x_point in range(shape[0]):
+        for y_point in range(shape[1]):
+            if list(fg_image[x_point][y_point]) != [0, 0, 0]:
+                x_points.append(x_point)
+                y_points.append(y_point)
+    fg_cut_image = fg_image[min(x_points):max(x_points), ]
 
-def get_random(distance):
-    """
-    param
-    	distance ： 缺口位置
-   	return
-    	track_list： 轨迹数组
-    """
+    bg_image = np.asarray(bytearray(requests.get(url=bg, verify=False).content), dtype='uint8')
+    bg_image = cv2.imdecode(bg_image, 1)
+    bg_cut_image = bg_image[min(x_points):max(x_points), ]
 
-    #初始轨迹
-    track_list = [[0,random.randint(-20,-2),0]]
-
-    #随机记录滑动次数
-    while True:
-        before_track = track_list[-1]
-        x = random.randint(0,40)
-        y = 0
-        z = random.randint(98,102)
-        new_x = x + before_track[0]
-        new_z = z + before_track[2]
-        if new_x >= distance:
-            track_list.append([distance, y, new_z])
-            break
-        else:
-            track_list.append([new_x,y,new_z])
-
-    for i in range(3):
-        before_track = track_list[-1]
-        z = random.randint(99, 101)
-        track_list.append([before_track[0], before_track[1], z + before_track[2]])
-
-    return track_list
-
+    result = cv2.matchTemplate(bg_cut_image, fg_cut_image, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+    distance = max_loc[0]
+    return int(distance)
 def cul_que():
-    det = ddddocr.DdddOcr(show_ad=False)
+    det = ddddocr.DdddOcr(show_ad=False,det=False,ocr=False)
     with open('bg.jpg','rb') as f:
         background_bytes =f.read()
     with open('fg.jpg','rb') as f:
         target_bytes =f.read()
-    res = det.slide_match(background_bytes,target_bytes,simple_target=True)
-    return res['target'][0]
-
+    res = det.slide_match(background_bytes=background_bytes,target_bytes=target_bytes,simple_target=True)
+    return res['target'][0]/2
+def get_trace_and_times(distance):
+    x = [0, 0]
+    y = [0, 0, 0]
+    z = [0]
+    count = np.linspace(-math.pi / 2, math.pi / 2, random.randrange(20, 30))
+    func = list(map(math.sin, count))
+    nx = [i + 1 for i in func]
+    add = random.randrange(10, 15)
+    sadd = distance + add
+    x.extend(list(map(lambda x: x * (sadd / 2), nx)))
+    x.extend(np.linspace(sadd, distance, 3 if add > 12 else 2))
+    x = [math.floor(i) for i in x]
+    for i in range(len(x) - 2):
+        if y[-1] < 30:
+            y.append(y[-1] + random.choice([0, 0, 1, 1, 2, 2, 1, 2, 0, 0, 3, 3]))
+        else:
+            y.append(y[-1] + random.choice([0, 0, -1, -1, -2, -2, -1, -2, 0, 0, -3, -3]))
+    for i in range(len(x) - 1):
+        z.append((z[-1] // 100 * 100) + 100 + random.choice([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2]))
+    trace = list(map(list, zip(x, y, z)))
+    times = trace[-1][-1] + random.randint(1, 5)
+    return trace, times
 headers = {
     "Accept": "*/*",
     "Accept-Language": "zh-CN,zh;q=0.9",
@@ -70,10 +82,8 @@ headers = {
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-platform": "\"Windows\""
 }
-
 url = "https://captcha1.fengkongcloud.cn/ca/v1/register"
 org = "d6tpAY1oV0Kv5jRSgxQr"
-
 params = {
     "lang": "zh-cn",
     "model": "slide",
@@ -81,30 +91,26 @@ params = {
     "organization": org,
     "sdkver": "1.1.3",
     "appId": "default",
-    "captchaUuid": "20250307022623mTWxDC3xi75pKKXy24",
+    "captchaUuid": "20250307052547MZa6fp5ySkhr6JRrxE",
     "rversion": "1.0.4",
-    "callback": "sm_1741286645601",
+    "callback": "sm_1741299467338",
     "channel": "DEFAULT"
 }
 session = requests.session()
 response = session.get(url, headers=headers, params=params)
-
 dict_jsonp = handle_jsonp(response.text)
 fg_url="https://castatic.fengkongcloud.cn"+dict_jsonp['detail']['fg']
 bg_url="https://castatic.fengkongcloud.cn"+dict_jsonp['detail']['bg']
 rid=dict_jsonp['detail']['rid']
+que=get_distance(fg_url,bg_url)
 
-download(fg_url,'fg.jpg',session)
-download(bg_url,'bg.jpg',session)
-
-que=cul_que() // 2
-gui = get_random(que)
+trace,time = get_trace_and_times(que/2)
 f = open("demo1.js","r",encoding="utf-8")
 js_code = f.read()
 f.close()
 ctx = js_code = execjs.compile(js_code)
-verify_param=ctx.call("cul",que,gui,org,rid)
+verify_param=ctx.call("cul",que/2,trace,org,rid,time)
 verify_url = "https://captcha1.fengkongcloud.cn/ca/v2/fverify"
 verify_response = session.get(verify_url, params=verify_param)
-print(verify_response.text)
 
+print(verify_response.text)
